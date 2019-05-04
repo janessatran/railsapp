d5487a3ed5c949c92a2cb4cdf4c7086db88efdac
Janessa Tran <tranjanessa@gmail.com>
2019-05-04 15:27:28 -0500

    Update static_pages#search to redirect to root when search bar is empty

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,10 @@
 class StaticPagesController < ApplicationController
-  def home
-  end
+  def home; end
 
   def search
+    if params[:search].blank?  
+      redirect_to(root_path, alert: "Empty field!") and return  
+    else  
+    end
   end
 end
