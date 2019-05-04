217c05cd574aa4b8e5c74d4171d8b3f60cc04a47
Janessa Tran <tranjanessa@gmail.com>
2019-05-04 15:57:23 -0500

    Add Topic model

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,11 @@ class StaticPagesController < ApplicationController
   def home; end
 
   def search
-    if params[:search].blank?  
-      redirect_to(root_path, alert: "Empty field!") and return  
-    else  
+    if params[:search].blank?
+      redirect_to(root_path, alert: 'Empty field!') && return
+    else
+      @parameter = params[:search].downcase
+      @results = Topic.all.where('lower(name) LIKE :search', search: "%#{@parameter}%")
     end
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+  test 'should get home' do
+    get root_path
     assert_response :success
   end
 
+  # test "should redirect to home when search bar empty" do
+  #   search_param = ''
+  #   params[:search] = search_param
+
+  # end
 end
