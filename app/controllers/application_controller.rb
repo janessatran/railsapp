d23d888eb1d699d2731a91edc0499f4d12489353
Janessa Tran <tranjanessa@gmail.com>
2019-05-12 17:10:57 -0500

    Implement basic login: changing layout when user is logged in so navbar has dropdown menu with settings and logout. Redirecting new user signups to profile page/

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  def home
-  end
+  protect_from_forgery with: :exception
+  include SessionsHelper
 end
---
+++ spec/requests/users_signups_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "UsersSignups", type: :request do
+  describe "GET /users_signups" do
+    it "works! (now write some real specs)" do
+      get signup_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "valid signup" do
+    it "creates the new user and redirects to profile" do
+      get signup_path
+      initial_count = User.count
+      expect do
+        post users_path, params: { user: { name:  "Example User",
+                                           email: "user@example.com",
+                                           password:              "password",
+                                           password_confirmation: "password" } }
+      end.to change { User.count }.from(initial_count).to(initial_count + 1)
+      follow_redirect!
+      expect(response).to render_template('users/show')
+      assert is_logged_in?
+    end
+  end
+end
