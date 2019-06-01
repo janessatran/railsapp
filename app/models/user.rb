49cfe0344b9f61b0cf38729a9d77ba695e0cc57a
Janessa Tran <tranjanessa@gmail.com>
2019-06-01 11:07:47 -0500

    Updated Gemfile for Heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
-
-  has_many :cheatsheets, dependent: :destroy
   attr_accessor :remember_token, :activation_token
   before_save   :downcase_email
   before_create :create_activation_digest
+  has_many :cheatsheets, dependent: :destroy
+
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
@@ -55,13 +55,18 @@ class User < ApplicationRecord
   # Sets the password reset attributes.
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest,  User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+    # update_attribute(:reset_digest,  User.digest(reset_token))
+    # update_attribute(:reset_sent_at, Time.zone.now)
   end
 
   # Sends password reset email.
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.describe UserMailer, type: :mailer do
   before do
     @user = create(:user)
+    @user.reset_token = User.new_token
   end
 
   describe "account_activation" do
@@ -19,18 +20,19 @@ RSpec.describe UserMailer, type: :mailer do
     end
   end
 
-  # describe "password_reset" do
-  #   let(:mail) { UserMailer.password_reset(@user) }
+  describe "password_reset" do
+    let(:mail) { UserMailer.password_reset(@user) }
 
-  #   it "renders the headers" do
-  #     expect(mail.subject).to eq("Password reset")
-  #     expect(mail.to).to eq([@user.email])
-  #     expect(mail.from).to eq(["noreply@example.com"])
-  #   end
+    it "renders the headers" do
+      expect(mail.subject).to eq("Password reset")
+      expect(mail.to).to eq([@user.email])
+      expect(mail.from).to eq(["noreply@example.com"])
+    end
 
-  #   it "renders the body" do
-  #     expect(mail.body.encoded).not_to be_blank
-  #   end
-  # end
+    it "sends a reset token" do
+      assert_match @user.reset_token, mail.body.encoded
+      assert_match CGI.escape(@user.email), mail.body.encoded
+    end
+  end
 
 end
